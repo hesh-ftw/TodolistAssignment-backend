3eb8d95b06e00faff5ef261ad39fb55d3e636aca
Chamath Heshan <chamathheshan076@gmail.com>
2025-04-01 23:33:50 +0530

    password reset via email

---
+++ src/main/java/com/example/TodoListBackend/Model/PasswordResetToken.java
@@ -0,0 +1,76 @@
+package com.example.TodoListBackend.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PasswordResetToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    private Instant expiryDate;
+    private boolean used;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    public PasswordResetToken(String token, Instant expiryDate, User user) {
+        this.token = token;
+        this.expiryDate = expiryDate;
+        this.user = user;
+    }
+
+    //getters and setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public Instant getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(Instant expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+    public boolean isUsed() {
+        return used;
+    }
+
+    public void setUsed(boolean used) {
+        this.used = used;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
---
+++ src/main/java/com/example/TodoListBackend/Service/EmailService.java
@@ -0,0 +1,22 @@
+package com.example.TodoListBackend.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+    @Autowired
+    JavaMailSender mailSender;
+
+    public void sendPasswordResetEmail(String toEmail, String resetUrl){
+        SimpleMailMessage message= new SimpleMailMessage();
+        message.setTo(toEmail);
+        message.setSubject("Password Reset Email ");
+        message.setText("Click the attached link to reset your password : "+ resetUrl);
+
+        mailSender.send(message);
+
+    }
+}
